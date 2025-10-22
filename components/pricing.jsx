ef8c13c5af5c04c89c02a762af80d7560daaf0bd
adarsh <adarshchaturvedi0207@gmail.com>
2025-10-23 00:37:24 +0530

    created pricing section

--- components/pricing.jsx
+++ components/pricing.jsx
@@ -1,8 +1,25 @@
+import { PricingTable } from '@clerk/nextjs'
 import React from 'react'
 
 const Pricing = () => {
     return (
-        <div>Pricing</div>
+        <section className='py-20' id='pricing'>
+            <div className='max-w-6xl mx-auto px-6'>
+                <div className="text-center mb-16">
+                    <h2 className="text-5xl font-bold  mb-6">
+                        Simple{" "}
+                        <span className='bg-gradient-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent'>
+                            Pricing
+
+                        </span>
+                    </h2>
+                    <p className="text-xl text-gray-300 ">
+                        Start free and upgrade as you grow. Our flexible pricing plans are designed to fit your needs, whether you're just getting started or scaling up your creative projects.
+                    </p>
+                </div>
+
+            </div>
+        </section>
     )
 }
 
